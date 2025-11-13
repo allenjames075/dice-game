80f2b21ae2b3000a853135acf7c3b160f6f31205
DMLI Dev 05 <dmlidev.05@gmail.com>
2025-11-13 13:49:14 +0800

    Just modified something

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-
 function rollDice () {
     let randomNumber1 = Math.floor(Math.random() * 6) + 1;
     let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-    let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
-    let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+    let randomImage1 = "images/dice" + randomNumber1 + ".png";
+    let randomImage2 = "images/dice" + randomNumber2 + ".png";
 
-    document.querySelector(".img1").setAttribute("src", randomImageSource1);
-    document.querySelector(".img2").setAttribute("src", randomImageSource2);
+    document.querySelector(".img1").setAttribute("src", randomImage1);
+    document.querySelector(".img2").setAttribute("src", randomImage2);
 
-    if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "Player 1 Wins!";
-    } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").textContent = "Player 2 Wins!";
+    if (randomImage1 > randomImage2) {
+        document.querySelector("h1").textContent = "Player 1 Wins!";
+    } else if (randomImage1 < randomImage2) {
+        document.querySelector("h1").textContent = "Player 2 Wins!"
     } else {
-    document.querySelector("h1").textContent = "Draw!";
+        document.querySelector("h1").textContent = "Draw!";
     }
 }
 
