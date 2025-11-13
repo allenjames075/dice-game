e242f25a8c2598e28aee7e8182b013a81bc48c04
DMLI Dev 05 <dmlidev.05@gmail.com>
2025-11-13 08:52:50 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function rollDice () {
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+    let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+    document.querySelector(".img1").setAttribute("src", randomImageSource1);
+    document.querySelector(".img2").setAttribute("src", randomImageSource2);
+
+    if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+    } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+    } else {
+    document.querySelector("h1").textContent = "Draw!";
+    }
+}
+
+document.getElementById("playBtn").addEventListener("click", rollDice);
